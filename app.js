b1b4d85c87464ca1afd2087aae0423a26cc751bd
Trevor <trevorjbaron@gmail.com>
2014-07-26 14:31:36 -0700

    fix non backwards compatable dependency and add shrinkwrap

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(router(app))
 
 //PAGE ROUTES
 app.get('/', defaultPageLoad('index'))
-app.get('/public/*', serve('.'))
+app.get(/\/public\/*/, serve('.'))
 
 //API ROUTES
 //app.get('/testUser', userCtrl.getUsers)
