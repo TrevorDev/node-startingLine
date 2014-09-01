82945f0c6b78137a4322417e0b4ad778f138f1c4
unknown <trevorjbaron@gmail.com>
2014-09-01 18:07:22 -0400

    added passport auth

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var config = require('./lib/config')
-//var database = require('./lib/database')
+    //var database = require('./lib/database')
 var logger = require('koa-logger')
 var router = require('koa-router')
 var serve = require('koa-static')
@@ -13,8 +13,12 @@ var https = require('https')
 var http = require('http')
 var request = require('request');
 var fs = require('fs')
+var passport = require('koa-passport'),
+    LocalStrategy = require('passport-local').Strategy;
 var app = koa()
-
+var auth = require('./lib/auth.js');
+var bodyParser = require('koa-bodyparser')
+var session = require('koa-sess')
 //Add database
 // si = database.getSequelizeInstance()
 //si.sync()
@@ -23,47 +27,70 @@ var app = koa()
 
 //REMOVE IN PRODUCTION??
 swig.setDefaults(config.templateOptions)
-
-//ROUTES
-app.keys = [config.sessionSecret]
+app.keys = ['your-session-secret']
 app.use(session())
+app.use(bodyParser())
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(jsonResp())
 app.use(router(app))
 
-//PAGE ROUTES
+//ROUTES --------------------------------------------------------------------------------------------------------------------
+
+//AUTH
+auth.setRoutes(app);
+
+//DEFAULTS
 app.get('/', defaultPageLoad('index'))
 app.get(/\/public\/*/, serve('.'))
+
+//SECURE
+var secured = new router()
+app.use(function*(next) {
+	console.log(this.session)
+  if (this.isAuthenticated()) {
+    yield next
+  } else {
+    this.redirect('/')
+  }
+})
+
+secured.get('/account',defaultPageLoad('account'))
+
+app.use(secured.middleware())
 
 //API ROUTES
 //app.get('/testUser', userCtrl.getUsers)
 
-//PAGE HANDLERS
+//PAGE HANDLERS ---------------------------------------------------------------------------------------------------------------------
 function defaultPageLoad(pageName, requiresLogin) {
-	return function *(){
-		/*if(requiresLogin===true && !sessionHelper.isLoggedIn(this.session)){
+    return function * () {
+        /*if(requiresLogin===true && !sessionHelper.isLoggedIn(this.session)){
 			this.redirect('/login')
 			return
 		}*/
-
-		var temp = {};
-		this.body = yield render(pageName, temp)
-	}
+        var temp = {};
+        this.body = yield render(pageName, temp)
+    }
 }
 
-function render(page, template){
-	return views(__dirname + '/view', config.templateOptions)(page, template)
+function render(page, template) {
+    return views(__dirname + '/view', config.templateOptions)(page, template)
 }
 
 var server = http.createServer(app.callback())
 
-//SOCKETIO
+
+
+
+//SOCKETIO ---------------------------------------------------------------------------------------------------------------------
 var io = require('socket.io').listen(server);
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
 });
 
 server.listen(config.appPort);
-console.log('Started ----------------------------------------------'+config.appPort)
+console.log('Started ----------------------------------------------' + config.appPort)
